e16f0a165caabdb08de617d6248540ed19884e68
danielwilliampeters <danielwilliampeters@outlook.com>
2026-03-09 21:07:05 +0000

    Refactor ring sizes and add a smaller size; bump version to 11

--- Core/Textures.lua
+++ Core/Textures.lua
@@ -5,72 +5,72 @@ local HC = HelloCursor
 
 HC.TEX = {
   RING = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_48_classic.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_64_classic.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_80_classic.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_96_classic.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_128_classic.tga",
   },
 
   RING_SMALL = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_small_48_classic.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_small_64_classic.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_small_80_classic.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_small_96_classic.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Classic\\ring_small_128_classic.tga",
   },
 
   NEON_RING = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_128.tga",
   },
 
   NEON_RING_SMALL = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_small_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_small_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_small_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_small_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_small_128.tga",
   },
 
   NEON_CORE = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_128.tga",
   },
 
   NEON_CORE_SMALL = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_small_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_small_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_small_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_small_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_core_small_128.tga",
   },
 
   NEON_INNER = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_128.tga",
   },
 
   NEON_INNER_SMALL = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_small_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_small_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_small_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_small_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_inner_small_128.tga",
   },
 
   NEON_EDGE = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_128.tga",
   },
 
   NEON_EDGE_SMALL = {
+    [48]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_small_48.tga",
     [64]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_small_64.tga",
     [80]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_small_80.tga",
     [96]  = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_small_96.tga",
-    [128] = "Interface\\AddOns\\HelloCursor\\Assets\\Rings\\Modern\\ring_edge_small_128.tga",
   },
 }
